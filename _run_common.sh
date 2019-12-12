f94208b2c98371f9547cc82462b65f19c68e2a19
B. van Berkum <dev@dotmpe.com>
2019-12-12 21:46:18 +0100

    Detect tty in wrappers

--- _run_common.sh
+++ _run_common.sh
@@ -5,7 +5,16 @@ for dev in /dev/ttyUSB*
 do
   devices="$devices --device $dev:$dev"
 done
-docker run -ti --rm -P \
+
+# Only allocate tty if we detect one
+if [ -t 0 -a -t 1 ]; then
+    DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -t"
+fi
+
+# Always set -i to support piped and terminal input in run/exec
+DOCKER_RUN_OPTIONS="$DOCKER_RUN_OPTIONS -i"
+
+docker run $DOCKER_RUN_OPTIONS --rm -P \
   $devices \
   -v $DCKR_VOL/arduino/tools:/home/arduino/.arduino15 \
   -v $PROJ_DIR/arduino-docs:/home/arduino/Arduino \
