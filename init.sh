6261923824eb521e870f55caee58f3ac48eb7c17
B. van Berkum <dev@dotmpe.com>
2019-12-15 18:54:23 +0100

    Testing

--- init.sh
+++ init.sh
@@ -2,7 +2,11 @@
 
 mkdir -vp $DCKR_VOL/arduino/tools $HOME/project/arduino-docs
 touch $DCKR_VOL/arduino/shell-history
-chown -R 1000:1000 $DCKR_VOL/arduino $HOME/project/arduino-docs
+
+./run.sh -c "id -u && id -g"
+uid=$(./run.sh -c "id -u")
+gid=$(./run.sh -c "id -g")
+chown -R $uid:$gid $DCKR_VOL/arduino $HOME/project/arduino-docs
 
 #./run.sh -c 'arduino-cli core update-index && \
 #  arduino-cli core install arduino:avr'
